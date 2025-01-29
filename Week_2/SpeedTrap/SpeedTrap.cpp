caced6e62bb840088d1e3324aad5d0a39dc2a889
nathan-huffman333 <nathan.huffman001@albright.edu>
2025-01-29 12:34:47 -0500

    1/29/2025

--- Week_2/SpeedTrap/SpeedTrap.cpp
+++ Week_2/SpeedTrap/SpeedTrap.cpp
@@ -1,9 +1,25 @@
 /*
     File name: SpeedTrap.cpp
-    Description: This program 
+    Description: This program uses conditional statements to check to see if a user is speeding based of the speed that they input.  
     Name: Nathan Huffman
     Date: 1/29/2025
 */
 
 #include <iostream>
 
+int main() {
+    int speed_limit;
+    double current_speed;
+
+    std::cout<<"Enter the speed limit: ";
+    std::cin>>speed_limit;
+    std::cout<<"Enter your current speed: ";
+    std::cin>>current_speed;
+
+    if (current_speed > speed_limit)
+        std::cout << "You are speeding, you are getting a speed ticket." << std::endl;
+    else
+        std::cout << "Your speed is legal, and you will not receive a ticket." << std::endl;
+    
+    return 0;
+}
