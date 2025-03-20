2b06780ae29e7a0eb991169eb7a207e325b31e83
nathan-huffman333 <nathan.huffman001@albright.edu>
2025-03-20 19:00:49 -0400

    3/20/2025

--- Week_9/Book Assignment/BookAssignment.cpp
+++ Week_9/Book Assignment/BookAssignment.cpp
@@ -1,3 +1,10 @@
+/*
+    File name: BookAssignment.cpp
+    Description: This program creates a calculator class that allows methods to be called to calculate return the sum, different, product, or quotient to a simple problem.
+    Name: Nathan Huffman
+    Date: 3/20/2025
+*/
+
 # include <iostream>
 
 enum class Operation {ADD, SUBTRACT, MULTIPLY, DIVIDE};
