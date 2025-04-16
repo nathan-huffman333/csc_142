b9ece34e6efac16580efbc6faf7dab45c0496532
nathan-huffman333 <nathan.huffman001@albright.edu>
2025-04-16 11:39:11 -0400

    4/16/2025

---
+++ Week_15/BookAssignment/12-1.cpp
@@ -0,0 +1,31 @@
+/*
+    File name: 12-1.cpp
+    Description: Reimplementation of the narrow_cast function from the book using std::optional.
+    Name: Nathan Huffman
+    Date: 4/16/2025
+*/
+
+#define CATCH_CONFIG_MAIN
+#include <stdexcept>
+#include <optional>
+#include "catch.hpp"
+
+template <typename To, typename From>
+std::optional<To> narrow_cast(From value) {
+	const auto converted = static_cast<To>(value);
+	const auto backwards = static_cast<From>(converted);
+	if (value != backwards) return std::nullopt;
+	return converted;
+};
+
+TEST_CASE("narrow cast reimplementation with std::optional") {
+	// no narrowing - pass test
+	int perfect{ 496 };
+	const auto perfect_short = narrow_cast<short>(perfect);
+	REQUIRE(perfect == perfect_short);
+	
+	// narrowing - fail test
+	int cyclic{ 142857 };
+	const auto cyclic_short = narrow_cast<short>(cyclic);
+	REQUIRE_FALSE(cyclic == cyclic_short);
+};
